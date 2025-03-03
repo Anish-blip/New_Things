338ba257f77767632a0c6c8a1e671dfa9f7b16b8
MariaAnish M <Maria.Anish@LTTS.com>
2025-03-03 15:32:11 +0530

    Deleted all files

--- script.py
+++
@@ -1,4 +0,0 @@
-d = {"key1": 1, "key2": 2, "key3": 1, "key4": 3, "key5": 1, "key6": 4, "key7": 2}
-
-for k in d:
-    print(f"key={k} value={d[k]}")
--- string.py
+++
@@ -1,7 +0,0 @@
-def double_char(s):
-    return ''.join([c*2 for c in s])
-
-# Examples
-print(double_char('The'))  # Output: 'TThhee'
-print(double_char('AAbb'))  # Output: 'AAAAbbbb'
-print(double_char('Hi-There'))  # Output: 'HHii--TThheerree'
